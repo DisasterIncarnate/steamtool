ebc073b0a4ddb48459f02d490cab9080c39f081d
DisasterIncarnate <77200514+DisasterIncarnate@users.noreply.github.com>
2024-08-20 01:49:38 +0100

    Add files via upload
    change some spelling and added auto-inserting terminal alias for flatpak protontricks.

--- steamtool.sh
+++ steamtool.sh
@@ -9,12 +9,13 @@ PROCESSCMD="False"
 # This is up and running as standard, mothing to install/setup, ready to use.
 TESTRAM="/dev/shm/SteamTool/RAMACTIVE"
 if ! test -f "$TESTRAM"; then
-  # File doesnt exist, so lets setup a RamDick and make the file Exist
+  # File doesnt exist, so lets setup a RamDisk and make the file Exist
   mkdir /dev/shm/SteamTool
   echo "OK" > /dev/shm/SteamTool/RAMACTIVE
 fi
 
 # Check that PATH includes $HOME/.local/bin
+CWARN="NONE"
 CHKA=$(cat $HOME/.profile | grep -i "HOME/.local/bin:" | grep -i "PATH")
 CHKB=$(cat $HOME/.bashrc | grep -i "HOME/.local/bin:" | grep -i "PATH")
 if [[ $CHKA == "" ]] && [[ $CHKB == "" ]]; then
@@ -24,12 +25,28 @@ if [[ $CHKA == "" ]] && [[ $CHKB == "" ]]; then
   echo 'fi' >> $HOME/.bashrc
   echo "PATH was not set to check $HOME/.local/bin for scripts/commands, this has been corrected."
   echo "** You need to close this terminal window and re-open for changes to take effect **"
-  exit
+  CWARN="YES"
 fi
 
 # Check user has the Flatpak ProtonTricks installed and give options if not found.
-CHKA=$(flatpak run com.github.Matoking.protontricks -V | grep -i "not installed")
-if [[ $CHKA != "" ]]; then
+CHKA=$(cat $HOME/.profile | grep -i "protontricks" | grep -i "alias")
+CHKB=$(cat $HOME/.bashrc | grep -i "protontricks" | grep -i "alias")
+if [[ $CHKA == "" ]] && [[ $CHKB == "" ]]; then
+  # Register alias for protontricks to launch flatpak protontricks
+  echo "alias protontricks='flatpak run com.github.Matoking.protontricks'" >> $HOME/.bashrc
+  echo "alias protontricks-launch='flatpak run --command=protontricks-launch com.github.Matoking.protontricks'" >> $HOME/.bashrc
+  CWARN="YES"
+fi
+
+if [[ $CWARN == "YES" ]]; then
+  echo Additions have been made to your $HOME/.bashrc file to make using this script and protontricks easier...
+  echo Close this terminal window and relaunch for changes to take effect...
+  echo
+  exit
+fi
+
+CHKC=$(protontricks -V | grep -i "not installed")
+if [[ $CHKC != "" ]]; then
   echo "This script requires Protontricks, Please install via the Discover Store or"
   echo "open konsole and type: flatpak install flathub com.github.Matoking.protontricks"
   echo ""
@@ -57,7 +74,7 @@ fi
 
 # Check for different Steam installs.
 # Find main LibraryFolder.VDF file location.
-MAINVDF=`find /home/$USER -maxdepth 6 -name "libraryfolders.vdf" | grep -i "steamapps"`
+MAINVDF=`find /home/$USER -maxdepth 8 -name "libraryfolders.vdf" | grep -i "steamapps"`
 if [[ ${1,,} == "--exe" ]] || [[ ${1,,} == "--open" ]] || [[ ${1,,} == "--run" ]]; then
   # Find common windows executable files.
   if [[ $2 == "" ]]; then
@@ -72,7 +89,8 @@ if [[ ${1,,} == "--exe" ]] || [[ ${1,,} == "--open" ]] || [[ ${1,,} == "--run" ]
       echo "You can only --run files if you have launched a game at least once to"
       echo "use the environment and APPID required by ProtonTricks which this script uses..."
       echo "------------------------------------------"
-      find "$xtrpath" -type f \( -iname \*.exe -o -iname \*.com -o -iname *.bat \) > /dev/shm/SteamTool/test-exelist
+      find "$xtrpath" -type f \( -iname \*.exe -o -iname \*.com -o -iname *.bat -o -iname \*.AppImage \) > /dev/shm/SteamTool/test-exelist
+      # find "$xtrpath" -type f \( -iname \*.exe -o -iname \*.com -o -iname *.bat \) > /dev/shm/SteamTool/test-exelist
       echo "INDEX   LOCATION"
       cat -b -n /dev/shm/SteamTool/test-exelist
     fi
@@ -92,7 +110,7 @@ if [[ ${1,,} == "--exe" ]] || [[ ${1,,} == "--open" ]] || [[ ${1,,} == "--run" ]
           lastexe=$(cat /dev/shm/SteamTool/test-lastexe)
           xtrid=$(sed -n "$lastexe"p /dev/shm/SteamTool/test-id)
           xtrexe=$(sed -n "$2"p /dev/shm/SteamTool/test-exelist)
-          WINEESYNC=1 WINEFSYNC=1 flatpak run --command=protontricks-launch com.github.Matoking.protontricks --appid "$xtrid" "$xtrexe" > $HOME/Documents/SteamTool-Exe-Log.txt 2>&1 &
+          WINEESYNC=1 WINEFSYNC=1 protontricks-launch --appid "$xtrid" "$xtrexe" > $HOME/Documents/SteamTool-Exe-Log.txt 2>&1 &
         else
           echo "Cannot --run something until you have used --exe to search for runnable files..."
           echo ""
@@ -124,7 +142,7 @@ if [[ ${1,,} == "--deps" ]]; then
       echo "Specify the Dependency you need to install...   (EG: corefonts)"
     else
       # Run protontricks and try to install dependencies
-      WINEESYNC=1 WINEFSYNC=1 flatpak run com.github.Matoking.protontricks "$xtrid" $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} #>/dev/null 2>&1
+      WINEESYNC=1 WINEFSYNC=1 protontricks "$xtrid" $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} #>/dev/null 2>&1
       echo ""
     fi
   fi
@@ -149,7 +167,7 @@ for file in $MAINVDF; do
   echo $base_dir | tr " " "\n" > /dev/shm/SteamTool/test-paths
 done
 
-basedir=$(find /home/$USER -maxdepth 6 -name "steamapps" | grep -i ".steam/")
+basedir=$(find /home/$USER -maxdepth 8 -name "steamapps" | grep -i ".steam/")
 
 GINDEX=0
 paths="/dev/shm/SteamTool/test-paths"
@@ -157,7 +175,7 @@ paths="/dev/shm/SteamTool/test-paths"
 # do clever stuff for compatdata/non-steam installed games and find start-in path which we cant get
 # from the usual text based .VDF file(s), as the .VDF file used for non-steam game locations/etc is binary.
 # and i am not smart enough to extract the information frmo a binary file, so we have this workaround.
-flatpak run com.github.Matoking.protontricks -l | grep -i "Non-Steam shortcut" | grep -i "$2" 1>/dev/shm/SteamTool/compat-paths 2>/dev/null
+protontricks -l | grep -i "Non-Steam shortcut" | grep -i "$2" 1>/dev/shm/SteamTool/compat-paths 2>/dev/null
 while IFS= read -r line; do
     newname=${line/"Non-Steam shortcut: "/}
     newname=${newname::-13}
@@ -174,7 +192,7 @@ while IFS= read -r line; do
         echo "$newname (Non-Steam)"
         echo $newid >> /dev/shm/SteamTool/test-id
         echo $newname >> /dev/shm/SteamTool/test-name
-        flatpak run com.github.Matoking.protontricks -c pwd $newid >> /dev/shm/SteamTool/test-locat
+        protontricks -c pwd $newid >> /dev/shm/SteamTool/test-locat
       else
         if [[ $2 == $GINDEX ]]; then
           echo "Match: $GINDEX $app_id $app_name"
